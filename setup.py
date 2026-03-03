7a770e9fd3d7e6d326fc6e71bc7d43f7658ea82a
UmangPatel <umang9505@gmail.com>
2026-03-03 23:55:44 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->list[str]:
+    '''
+    This function will return the list of requirements mentioned in the requirements.txt file
+    '''
+
+    requirements = []
+
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+    
+
+
+setup( 
+     
+    name = 'ML_Project',
+    version = '0.0.1',
+    author = 'UmangPatel',
+    author_email = '',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+ 
+)
